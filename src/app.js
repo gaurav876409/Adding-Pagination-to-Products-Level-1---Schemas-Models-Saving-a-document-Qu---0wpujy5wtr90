9a0ed4a3219f77e23dd386eac802d2640ab481f8
gaurav876409 <gaurav876409@gmail.com>
2023-07-25 11:24:16 +0530

    up

--- src/app.js
+++ src/app.js
@@ -27,11 +27,35 @@ app.use(express.json());
 
 app.get("/",async function(req,res){
 
-    var ids = [];
+    // var ids = [];
     
     //Write your Code here.
 
-    res.send(ids);
+    // res.send(ids);
+    try {
+        let limit = parseInt(req.query.limit) || 5;
+        let offset = parseInt(req.query.offset) || 0;
+    
+        // Ensure the limit does not exceed the maximum value (5 in this case)
+        limit = Math.min(limit, 5);
+    
+        // Calculate the skip value based on the offset and limit
+        const skip = offset * limit;
+    
+        // Find the products in the specified range using the limit and skip values
+        const productsInRange = await products
+          .find()
+          .skip(skip)
+          .limit(limit)
+          .select("_id");
+    
+        // Extract the _id values from the productsInRange and store them in the ids array
+        const ids = productsInRange.map((product) => product._id);
+    
+        res.send(ids);
+      } catch (err) {
+        res.status(500).send("Internal Server Error");
+      }
 
 });
 
